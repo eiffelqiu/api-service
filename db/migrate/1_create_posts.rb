a5bded7466a1df4aa4a10464013aa01bbcab5902
Eiffel Qiu <eiffelqiu@gmail.com>
2013-06-10 20:50:48 +0800

    refactor

--- db/migrate/1_create_posts.rb
+++ db/migrate/1_create_posts.rb
@@ -2,8 +2,10 @@ class CreatePosts < ActiveRecord::Migration
   
   def change
     create_table :posts do |t|
-      t.string :title
-      t.text :body
+      t.string :usrname
+      t.text :message
+      t.string :app
+      t.string :ip
       t.timestamps
     end
   end
